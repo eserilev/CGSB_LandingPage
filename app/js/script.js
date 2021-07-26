fab9013022d2f6ba8a792d53ad0dd61f79ba53f5
Eitan Seri-Levi <eserilev@ucsc.edu>
2021-07-25 22:32:54 -0700

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,28 @@
+const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
+
+btnHamburger.addEventListener('click', function() {
+
+    if(header.classList.contains('open')) {      
+        // close menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        
+        fadeElems.forEach(elem => {
+            elem.classList.remove('fade-in');
+            elem.classList.add('fade-out')
+        });
+    }  else { 
+        // open menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+
+        fadeElems.forEach(elem => {
+            elem.classList.add('fade-in');
+            elem.classList.remove('fade-out')
+        });
+    }
+});
